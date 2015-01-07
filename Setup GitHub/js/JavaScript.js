bcf7a25cee3332ce8940dcdda120f4366cf4f7d0
brbever <brbever@fullsail.edu>
2015-01-06 22:32:03 -0500

    Prompted user for name
    Prompted user for name

--- Setup GitHub/js/JavaScript.js
+++ Setup GitHub/js/JavaScript.js
@@ -4,4 +4,10 @@ Test JS file
 1-6-2015
 */
 
-alert("Testing 1, 2, 3");
+//alert("Testing 1, 2, 3");
+
+
+console.log("Testing the Console Log");
+
+var userInput = prompt("Type in your name");
+console.log(userInput);
