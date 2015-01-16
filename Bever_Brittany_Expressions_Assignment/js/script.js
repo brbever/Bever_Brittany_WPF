b8f680ac49c89206fc281b383ec6c8414f10a4e4
brbever <brbever@fullsail.edu>
2015-01-15 21:41:40 -0500

    comments and array
    Completed comments and made an array.

--- Bever_Brittany_Expressions_Assignment/js/script.js
+++ Bever_Brittany_Expressions_Assignment/js/script.js
@@ -7,13 +7,19 @@ var priceOfGas = prompt("Please enter the price of gas");
 var gallonsNeeded = prompt("Please enter the number of gallons of gas needed");
 //collect the number of gallons of gas to be purchased.
 
+
 groceryStorePoints /= 1000;
 console.log(groceryStorePoints);
-
+//grocery store points were divided by 1000 to figure out how many cents off per gallon are being used.
 
 priceOfGas -= groceryStorePoints;
 console.log(priceOfGas);
+//subtract the grocery store points from the price of gas to get what the price of gas would be after the grocery store points were taken out.
 
-var total = gallonsNeeded * priceOfGas;
+//Create and array of totals
+var totals = [gallonsNeeded, priceOfGas];
+
+var total = totals[0] * totals[1];
 console.log(total);
+//multiply the gallons of gas needed by the price of gas with the grocery store points to get the total price.
 
