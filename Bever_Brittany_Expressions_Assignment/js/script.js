13bd8423b5ca3a900e4dd8108f5e4ac42e3f7620
brbever <brbever@fullsail.edu>
2015-01-15 20:48:58 -0500

    Declared Variables
    Declared and tested variables and prompts.

--- Bever_Brittany_Expressions_Assignment/js/script.js
+++ Bever_Brittany_Expressions_Assignment/js/script.js
@@ -1,2 +1,5 @@
 // Bever_Brittany_Expressions_Assignment
 
+var groceryStorePoints = prompt("We will be calculating how much it will cost to buy gas using grocery store rewards points. \nPlease enter the number of points");
+var priceOfGas = prompt("Please enter the price of gas");
+var gallonsNeeded = prompt("Please enter the number of gallons of gas needed");
