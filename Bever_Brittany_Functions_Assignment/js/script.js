9c16da2866a5535edaa2d21f7377273d9543b6e9
brbever <brbever@fullsail.edu>
2015-01-29 22:35:16 -0500

    add comments
    add detailed comments

--- Bever_Brittany_Functions_Assignment/js/script.js
+++ Bever_Brittany_Functions_Assignment/js/script.js
@@ -1,43 +1,43 @@
 //Bever_Brittany_Functions_Assignment
 
-var diapersPerBox = prompt("Please enter the number of diapers in the box");
+var diapersPerBox = prompt("Please enter the number of diapers in the box");//define and prompt for the variable.
 while (diapersPerBox == "") {
-	diapersPerBox = prompt("Oops! you forgot to enter the number of diapers in the box");
-}
+	diapersPerBox = prompt("Oops! you forgot to enter the number of diapers in the box");//Use the while loop to make sure there is input for the variable.
+} 
 
-var diapersPerDay = prompt("Please enter the number of diapers used per day");
+var diapersPerDay = prompt("Please enter the number of diapers used per day");//define and prompt for the variable.
 while (diapersPerDay == "") {
-	diapersPerDay = prompt("Oops! you forgot to enter the number of diapers used per day");
+	diapersPerDay = prompt("Oops! you forgot to enter the number of diapers used per day");//Use the while loop to make sure there is input for the variable.
 }
 
-var boxPrice = prompt("Please enter the price of the box of diapers");
+var boxPrice = prompt("Please enter the price of the box of diapers");//define and prompt for the variable.
 while (boxPrice == "") {
-	boxPrice = prompt("Oops! you forgot to enter the price of the box of diapers");
+	boxPrice = prompt("Oops! you forgot to enter the price of the box of diapers");//Use the while loop to make sure there is input for the variable.
 }
-
+//define the fuction for calculating price of diapers per day.
 var calcDayPrice = function(b, d, p) {
 	daysPerBox = b / d;
 	pricePerDay = p / daysPerBox;
 	return pricePerDay.toFixed(2);
-}
+}//calculate the price per day by dividing diapers per box by diapers per day, then dividing the price of a box by the number of days per box.
 
-var dayPrice = calcDayPrice(diapersPerBox, diapersPerDay, boxPrice);
+var dayPrice = calcDayPrice(diapersPerBox, diapersPerDay, boxPrice);//define the variable for the day price. define the arguements
 
-console.log("The price of diapers per day is $" + dayPrice);
+console.log("The price of diapers per day is $" + dayPrice);//log the price of diapers per day
 
-var monthTotals = monthCalc(dayPrice);
+var monthTotals = monthCalc(dayPrice);//define the variable of month totals
 
 function monthCalc(d){
 	monthPrice = d * 30;
 	return monthPrice.toFixed(2);
-}
+}//calculate the month price by multiplying the day price by 30
 
-console.log("The price for diapers per month is $" + monthTotals);
+console.log("The price for diapers per month is $" + monthTotals);//log the month price
 
-var yearTotals = yearCalc(monthTotals);
+var yearTotals = yearCalc(monthTotals);//define the variable of year totals
 
 function yearCalc(m){
 	yearPrice = m * 12;
 	return yearPrice.toFixed(2);
-}
-console.log("The price for diapers per year is $" + yearTotals);
+}//calculate the month price by multiplying the month totals by 12.
+console.log("The price for diapers per year is $" + yearTotals);//log the year price
