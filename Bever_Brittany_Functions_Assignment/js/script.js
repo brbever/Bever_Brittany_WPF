7a641835fcce62ed47094aa8843574377ece7d62
brbever <brbever@fullsail.edu>
2015-01-29 21:49:35 -0500

    defined prompts
    defined prompts for the information needed.

---
+++ Bever_Brittany_Functions_Assignment/js/script.js
@@ -0,0 +1,19 @@
+//Bever_Brittany_Functions_Assignment
+
+var diapersPerBox = prompt("Please enter the number of diapers in the box");
+while (diapersPerBox == "") {
+	diapersPerBox = prompt("Oops! you forgot to enter the number of diapers in the box");
+}
+
+var diapersPerDay = prompt("Please enter the number of diapers used per day");
+while (diapersPerDay == "") {
+	diapersPerDay = prompt("Oops! you forgot to enter the number of diapers used per day");
+}
+
+var boxPrice = prompt("Please enter the price of the box of diapers");
+while (boxPrice == "") {
+	boxPrice = prompt("Oops! you forgot to enter the price of the box of diapers");
+}
+
+
+
