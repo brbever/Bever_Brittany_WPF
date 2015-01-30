be8e1f06cbd7a5840f8120026ef9c091d1447ef4
brbever <brbever@fullsail.edu>
2015-01-29 22:09:25 -0500

    added functions
    added and tested functions.

--- Bever_Brittany_Functions_Assignment/js/script.js
+++ Bever_Brittany_Functions_Assignment/js/script.js
@@ -15,5 +15,29 @@ while (boxPrice == "") {
 	boxPrice = prompt("Oops! you forgot to enter the price of the box of diapers");
 }
 
+var calcDayPrice = function(b, d, p) {
+	daysPerBox = b / d;
+	pricePerDay = daysPerBox / p;
+	return pricePerDay.toFixed(2);
+}
 
+var dayPrice = calcDayPrice(diapersPerBox, diapersPerDay, boxPrice);
 
+console.log("The price of diapers per day is $" + dayPrice);
+
+var monthTotals = priceCalc(dayPrice);
+
+function priceCalc(d){
+	monthPrice = d * 30;
+	return monthPrice;
+}
+
+console.log("The price for diapers per month is $" + monthPrice);
+
+var yearTotals = priceCalc(dayPrice);
+
+function priceCalc(d){
+	yearPrice = yearPrice * 12;
+	return yearPrice;
+}
+console.log("The price for diapers per year is $" + yearPrice);
